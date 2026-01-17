5327c82637060b88c781b40a63755085a9447f50
GauravAhuja05 <gauravA945@gmail.com>
2026-01-17 18:51:04 +0530

    17/jan/2026

---
+++ Controllers/VoiceAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JwtApp.Controllers
+{
+    public class VoiceAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
